63d8283954ab90e8eb9cdf34abf6381ad1dd415d
Karolina Barnik <karolinabarnik@gmail.com>
2021-02-11 20:51:09 +0100

    Add playerMove

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = 'papier';
+
+printMessage('Zagrałeś' + playerMove + '! Jeśli mój ruch to kamień, to wygrywasz!'
+        ');
